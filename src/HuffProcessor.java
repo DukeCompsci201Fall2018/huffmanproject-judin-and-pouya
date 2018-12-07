ca2a4b574375ce604d6c6923bb1d57df56440c13
pmohamma <pmohamma@trinity.edu>
2018-12-07 13:24:01 -0500

    working

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -59,7 +59,12 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
+		
+		int bits = in.readBits(BITS_PER_INT);
+		if (bits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with " + bits);
+		}
+		
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
